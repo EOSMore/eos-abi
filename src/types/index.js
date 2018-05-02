a21ebe82f90a1c6b9fdf7a2e31b39f0a742f0043
shrimp <liu.shrimp@gmail.com>
2018-05-02 10:24:06 +0800

    signed integer

--- src/types/index.js
+++ src/types/index.js
@@ -1,3 +1,7 @@
+import int8 from './int8';
+import int16 from './int16';
+import int32 from './int32';
+import int64 from './int64';
 import name from './name';
 import public_key from './public_key';
 import string from './string';
@@ -9,6 +13,10 @@ import uint64 from './uint64';
 import uint128 from './uint128';
 
 const types = {
+  int8,
+  int16,
+  int32,
+  int64,
   name,
   public_key,
   string,
